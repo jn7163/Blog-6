d1f61922b387bfba36a7af40306f32225d6ce6e4
WincerChan <WincerChan@jxnu.edu.cn>
2017-09-25 16:08:10 +0800

    Add coverage tests

--- app/models.py
+++ app/models.py
@@ -368,7 +368,7 @@ class Comment(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     disabled = db.Column(db.Boolean)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    port_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
 
     @staticmethod
     def on_changed_body(target, value, oldvalue, initiator):
@@ -380,6 +380,25 @@ class Comment(db.Model):
                 markdown(value, output_formal='html'),
                 tags=allowed_tags,
                 strip=True))
+
+    def to_json(self):
+        json_comment = {
+            'url': url_for('api.get_comment', id=self.id, _external=True),
+            'post': url_for('api.get_post', id=self.post_id, _external=True),
+            'body': self.body,
+            'body_html': self.body_html,
+            'timestamp': self.timestamp,
+            'author': url_for(
+                'api.get_user', id=self.author_id, _external=True),
+        }
+        return json_comment
+
+    @staticmethod
+    def from_json(json_comment):
+        body = json_comment.get('body')
+        if body is None or body == '':
+            raise ValidationError('comment does not have a body')
+        return Comment(body=body)
 
 
 db.event.listen(Comment.body, 'set', Comment.on_changed_body)
--- manage.py
+++ manage.py
@@ -7,6 +7,12 @@ from flask_script import Manager, Shell
 from app import create_app, db
 from app.models import Role, User
 
+COV = None
+if os.environ.get('BLOG_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
@@ -21,11 +27,25 @@ manager.add_command('db', MigrateCommand)
 
 
 @manager.command
-def test():
+def test(coverage=False):
     """Run the unit tests"""
+    if coverage and not os.environ.get('BLOG_COVERAGE'):
+        import sys
+        os.environ['BLOG_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://{}/index.html'.format(covdir))
+        COV.erase()
 
 
 if __name__ == '__main__':
